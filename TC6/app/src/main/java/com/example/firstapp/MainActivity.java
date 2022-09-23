ccc7f92e8430fb7ac0f8ebbf7d6444f4a0432883
Manuel Cabrera <manuel.968762062@gmail.com>
2022-09-23 01:10:49 -0500

    add TC6

--- TC6/app/src/main/java/com/example/firstapp/MainActivity.java
+++ TC6/app/src/main/java/com/example/firstapp/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.firstapp.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +19,75 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        events();
+        setListener();
     }
 
-    private void events(){
-        binding.txtTextView
+    private void setListener() {
+        binding.btnCalculate.setOnClickListener(listener -> {
+            /**
+             * Get Student
+             */
+            String student = binding.nameEditText.getText().toString().trim();
+            if (student.isEmpty()) {
+
+                Toast.makeText(this, getString(R.string.required_name), Toast.LENGTH_LONG).show();
+                return;
+            } else {
+                student = getString(R.string.student) + student + "\n";
+            }
+
+            /**
+             * Get Course
+             */
+            String course = getCourseSelected();
+            if (course.isEmpty()) {
+                Toast.makeText(this, getString(R.string.required_course), Toast.LENGTH_LONG).show();
+                return;
+            } else {
+                course = getString(R.string.course) + course + "\n";
+            }
+
+            /**
+             * Get Total
+             */
+            String total = getString(R.string.discount) + getTotalDiscounts() + "%\n" + getString(R.string.total) + getPriceTotal();
+
+            /**
+             * Print Result
+             */
+            binding.resultTextView.setText(student + course + total);
+        });
+    }
+
+    private String getCourseSelected() {
+        if (binding.radioNet.isChecked()) {
+            return "Net";
+        } else if (binding.radioJava.isChecked()) {
+            return "Java";
+        } else if (binding.radioPhp.isChecked()) {
+            return "PHP";
+        } else {
+            return "";
+        }
+    }
+
+    private double getPriceTotal() {
+        double priceCourse = binding.radioNet.isChecked() ? 500 : binding.radioJava.isChecked() ? 400 : 300;
+        if (getTotalDiscounts() > 0) {
+            return priceCourse - priceCourse * getTotalDiscounts() / 100;
+        } else {
+            return priceCourse;
+        }
+    }
+
+    private int getTotalDiscounts() {
+        int totalDiscounts = 0;
+        if (binding.chkDiscountFive.isChecked()) {
+            totalDiscounts += 5;
+        }
+        if (binding.chkDiscountTen.isChecked()) {
+            totalDiscounts += 10;
+        }
+        return totalDiscounts;
     }
 }
